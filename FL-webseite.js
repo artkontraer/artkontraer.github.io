3c9a83acc028b5cc7479943c1445dca3a5b81185
artkontraer <t@artkontraer.com>
2022-04-16 15:06:09 +0200

    logo shrink

--- FL-webseite.js
+++ FL-webseite.js
@@ -1,5 +1,32 @@
-document.addEventListener('scroll',() => {
-    document.getElementById("logo-klein").style.transform = "scale(0.5,0.5)";
-    
-});
 
+//  window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+    // document.addEventListener('scroll',() => {
+    // document.getElementById("logo-klein").style.transform = "scale(0.5,0.5)";
+    // document.getElementById("logo-klein").style.transition = "width, 1s";
+    // });
+// };
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 10) {
+    document.getElementById("logo-klein").style.transform = "scale(0.5,0.5)";
+    document.getElementById("logo-klein").style.transition = "width, 0.5s";
+  } 
+  else {
+    document.getElementById("logo-klein").style.transform = "scale(1,1)";
+    document.getElementById("logo-klein").style.transition = "width, 0.7s";
+  }
+}
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop < 30 || document.documentElement.scrollTop < 30) {
+//     document.getElementById("logo-klein").style.transform = "scale(1,1)";
+//     document.getElementById("logo-klein").style.transition = "width, 1s";
+//   } 
+// }
